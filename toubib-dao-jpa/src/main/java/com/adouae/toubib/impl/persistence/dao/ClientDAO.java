6ebf40f7e06487998c85e53358d45c22e1bf7e83
tallshadow-3.local <adil.haouas@.com>
2018-08-20 02:20:16 +0100

    first commit

--- toubib-dao-jpa/src/main/java/com/adouae/toubib/impl/persistence/dao/ClientDAO.java
+++
@@ -1,8 +0,0 @@
-package com.adouae.toubib.impl.persistence.dao;
-
-import com.adouae.toubib.impl.persistence.entity.Client;
-
-public interface ClientDAO {
-	public Long addPatient(Client clt);
-	//public Cities getCities(String country);
-}
--- toubib-dao-jpa/src/main/java/com/adouae/toubib/impl/persistence/dao/RoleDAO.java
+++
@@ -1,7 +0,0 @@
-package com.adouae.toubib.impl.persistence.dao;
-
-import com.adouae.toubib.impl.persistence.entity.Role;
-
-public interface RoleDAO {
-	public Integer addRole(Role role);
-}
